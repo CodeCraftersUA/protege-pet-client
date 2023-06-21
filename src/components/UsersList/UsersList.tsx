54769c66dc4297c43e1e66a4ea54465d63290d10
wesleymarchi <wesleymarchi.x3@gmail.com>
2023-06-21 11:45:22 -0300

    Table Users

---
+++ src/components/UsersList/UsersList.tsx
@@ -0,0 +1,49 @@
+import { IUser } from "./UserList";
+import "./UserList.style.css"
+
+type Props = {
+    list: IUser[]
+}
+
+const UserList = (props: Props) => {
+    const {list} = props;
+
+    return (
+        <>
+            <div>User List Page
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Sobrenome</th>
+                        <th>E-mail</th>
+                        <th>Município</th>
+                        <th>Estado</th>
+                        <th>Status</th>
+                    </tr>
+                    {list.map((user) => {
+                        console.log(user);
+                        return (
+                            <tr key={user.id}>
+                                <td>{`${user.id}`}</td>
+                                <td>{`${user.firstName}`}</td>
+                                <td>{`${user.lastName}`}</td>
+                                <td>{`${user.email}`}</td>
+                                <td>{`${user.municipio}`}</td>
+                                <td>{`${user.estado}`}</td>
+                                <td>
+                                    <div>
+                                        <span><input className="button-success" type="button" value="Aprovar" /> </span>
+                                        <span><input className="button-error" type="button" value="Reprovar" /> </span>
+                                    </div>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </table>
+            </div>
+        </>
+    );
+};
+
+export default UserList;
