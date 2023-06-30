fda4af42f79c4101d6063bdd2bc8bd6640fccd9d
wesleymarchi <wesleymarchi.x3@gmail.com>
2023-06-30 01:22:02 -0300

    New landing page

---
+++ src/Pages/NewLandingPage/index.tsx
@@ -0,0 +1,11 @@
+import Home from "../../components/NewLandingPage/Home/Home";
+
+const NewLandingPage = () => {
+    return (
+        <>
+           <Home />
+        </>
+    );
+};
+
+export default NewLandingPage;
