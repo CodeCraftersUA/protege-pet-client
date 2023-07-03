e1e09a8700edf3614c6e046ee1b77721eba4b9dd
wesleymarchi <wesleymarchi.x3@gmail.com>
2023-07-03 18:29:46 -0300

    Unauthorized animals

--- src/components/AnimalsList/NewAnimalModal/NewAnimalModal.tsx
+++ src/components/AnimalsList/NewAnimalModal/NewAnimalModal.tsx
@@ -11,7 +11,7 @@ const NewAnimalModal = () => {
                     <div className="modal-dialog">
                         <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="staticBackdropLabel">Novo Protetor</h1>
+                            <h1 className="modal-title fs-5" id="staticBackdropLabel">Novo Animal</h1>
                             <button type="button" className="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
